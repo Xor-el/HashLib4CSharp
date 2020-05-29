fb761801d51a8ff689b9f49c979cce3cafb218e7
Ugochukwu Mmaduekwe <ugo4brain@gmail.com>
2020-05-29 11:24:24 +0100

    simplify file header according to C# specs recommendation

--- HashLib4CSharp/src/Enum/HashRounds.cs
+++ HashLib4CSharp/src/Enum/HashRounds.cs
@@ -1,18 +1,14 @@
 /*
-(* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& *)
-{ *                             HashLib4CSharp Library                              * }
-{ *                      Copyright (c) 2020 Ugochukwu Mmaduekwe                     * }
-{ *                 GitHub Profile URL <https://github.com/Xor-el>                  * }
+HashLib4CSharp Library
+Copyright (c) 2020 Ugochukwu Mmaduekwe
+GitHub Profile URL <https://github.com/Xor-el>
 
-{ *  Distributed under the MIT software license, see the accompanying LICENSE file  * }
-{ *          or visit http://www.opensource.org/licenses/mit-license.php.           * }
+Distributed under the MIT software license, see the accompanying LICENSE file
+or visit http://www.opensource.org/licenses/mit-license.php.
 
-{ *                              Acknowledgements:                                  * }
-{ *                                                                                 * }
-{ *   This library was sponsored by Sphere 10 Software (https://www.sphere10.com)   * }
-{ *         for the purposes of supporting the XXX (https://YYY) project.           * }
-{ *                                                                                 * }
-(* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& *)
+Acknowledgements:
+This library was sponsored by Sphere 10 Software (https://www.sphere10.com)
+for the purposes of supporting the XXX (https://YYY) project.
 */
 
 namespace HashLib4CSharp.Enum
